5bcc956e5833a9032a82a509607d324b99eef97e
sos <92243200+Cyrilsos@users.noreply.github.com>
2023-09-18 14:28:56 +0530

    admin finished
    some css adjustment left rest in finished and can be deployed in the main for testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,12 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        pop: ["poppins"],
+      },
+    },
+  },
+  plugins: [],
+};
